7c14235b30348b34f82134dba76e01fda7e2e450
ksso730 <ksso7300@gmail.com>
2021-09-15 00:28:12 +0900

    middleware 실습

--- src/server.js
+++ src/server.js
@@ -1,42 +1,41 @@
 import express from "express";
+import morgan from "morgan";
 
 // *express application생성
 const app = express();
 
+// middleware
+const logger = morgan("dev");
 
-// middleware??
-
-// request와 response 중간에 있다.
-// request - middleware - response
-
-// 모든 handler는 controller 이며 middleware 이다.
-// -> next 변수: 
-// const handleEvnt = (req, res, next) => { next(); };
-const middlewr = (req, res, next) => {
-    console.log("middle here");
+const privateMiddlewr = (req, res, next) => {
+    const url = req.url;
+    if(url==="/protected"){
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    console.log("allowed, you may continue.");
     next();
 };
+
 
 // *요청기능
 const handleEvnt = (req, res) => {
     console.log("handleEvnt here");
-    return res.send("<h1>ByeBye</h1>");
+    return res.send("<h1>Home</h1>");
 };
 
-const handleLogin = (req, res) => {
-    return res.send("Login Please");
+const handleProtected = (req, res) => {
+    return res.send("This site is private.")
 };
 
-app.get("/", middlewr, handleEvnt);
-app.get("/login", handleLogin);
+app.use(logger);
+app.use(privateMiddlewr);
+app.get("/", handleEvnt);
+app.get("/protected", handleProtected);
 
 // *외부서버 연결
 const PORT = 4001
-const handleListening = () =>
-    console.log(`✅ Server listening on port http://localhost:${PORT}`);
-app.listen(PORT, handleListening);
 // callback 함수? 서버가 시작할때 작동하는 함수
 // port정보를 줘야함.
-
-// HTTP? 가장 안정적이고 오래된 방식. 서버와 통신하는.
-// 브라우저가 대신해서 http request 만들어줌
+const handleListening = () =>
+console.log(`✅ Server listening on port http://localhost:${PORT}`);
+app.listen(PORT, handleListening);
