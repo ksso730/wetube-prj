0bfcdbd14e19743c2ef92a7d10faf0b67de57434
ksso730 <ksso7300@gmail.com>
2021-09-27 18:30:59 +0900

    wetube_ edit video data

--- src/contorllers/videoController.js
+++ src/contorllers/videoController.js
@@ -8,39 +8,54 @@ import Video from "../models/Video";
 
 export const home = async(req, res) => {
     const videos = await Video.find({});
-    return res.render("home", {pageTitle: "Home", videos});
+    return res.render("home", { pageTitle: "Home", videos });
 }
 
-export const watch = (req, res) => {
-    const {id} = req.params;
-    return res.render("watch", {pageTitle: `Watch `, videos: []});
+export const watch = async(req, res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
-    const {id} = req.params;
-    return res.render("edit", {pageTitle: `Editing: `, videos: []});
+export const getEdit = async(req, res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
-    const {id} = req.params;
-    const {title} = req.body;
-    videos[id-1].title = title;
+export const postEdit = async(req, res) => {
+    const { id } = req.params;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    video.title = title;
+    video.description = description;
+    video.hashtags = hashtags.split(",").map((word) => word.startsWith('#') ? word : `#${word}`);
+    await video.save();
     return res.redirect(`/videos/${id}`);
 };
 
 export const getUpload = (req, res) => {
-    return res.render("upload", {pageTitle: "Upload Video"});
+    return res.render("upload", { pageTitle: "Upload Video" });
 }
 
 export const postUpload = async(req, res) => {
-    const {title, description, hashtags} = req.body;
-    try{
+    const { title, description, hashtags } = req.body;
+    try {
         await Video.create({
             title,
             description,
             hashtags: hashtags.split(",").map((word) => `#${word}`),
         });
-    }catch(error){
+    } catch (error) {
         console.log(error);
         return res.render("upload", {
             pageTitle: "Upload Video",
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { 
-    getEdit, postEdit, watch, getUpload, postUpload
+import {
+    getEdit,
+    postEdit,
+    watch,
+    getUpload,
+    postUpload
 } from "../contorllers/videoController";
 
 const videoRouter = express.Router();
@@ -8,7 +12,7 @@ const videoRouter = express.Router();
 // upload 를 2번째로 내리면, request는 가장 위에서부터 요청을 처리하기 때문에 upload가 id라고 인식한다.
 // 하지만 가장 상단으로 올려주면 정상적으로 작동한다.
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+// 문서에 의하면 몽고디비 id는 24바이트 16진수이며 24자리 str이다.: [0-9a-f]{24}
 export default videoRouter;
