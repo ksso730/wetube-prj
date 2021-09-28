bcf2fa637db0acb99823bc2fd149d50c504a6c05
ksso730 <ksso7300@gmail.com>
2021-09-28 16:27:19 +0900

    wetube search 기능 수정

--- src/contorllers/videoController.js
+++ src/contorllers/videoController.js
@@ -7,12 +7,12 @@ import Video from "../models/Video";
 //     });
 // }
 
-export const home = async (req, res) => {
-    const videos = await Video.find({}).sort({createdAt: "desc"});
+export const home = async(req, res) => {
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", { pageTitle: "Home", videos });
 }
 
-export const watch = async (req, res) => {
+export const watch = async(req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
     if (!video) {
@@ -21,7 +21,7 @@ export const watch = async (req, res) => {
     return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = async (req, res) => {
+export const getEdit = async(req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
     if (!video) {
@@ -30,7 +30,7 @@ export const getEdit = async (req, res) => {
     return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
 
-export const postEdit = async (req, res) => {
+export const postEdit = async(req, res) => {
     const { id } = req.params;
     const { title, description, hashtags } = req.body;
     const video = await Video.exists({ _id: id });
@@ -49,7 +49,7 @@ export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload Video" });
 }
 
-export const postUpload = async (req, res) => {
+export const postUpload = async(req, res) => {
     const { title, description, hashtags } = req.body;
     try {
         await Video.create({
@@ -68,16 +68,16 @@ export const postUpload = async (req, res) => {
     return res.redirect("/");
 };
 
-export const deleteVideo = async(req,res) => {
-    const {id} = req.params;
+export const deleteVideo = async(req, res) => {
+    const { id } = req.params;
     await Video.findByIdAndDelete(id);
     return res.redirect("/");
 };
 
 export const search = async(req, res) => {
-    const {keyword} = req.query;
-    let videos= [];
-    if(keyword){
+    const { keyword } = req.query;
+    let videos = [];
+    if (keyword) {
         videos = await Video.find({
             title: {
                 $regex: new RegExp(keyword, "ig"),
