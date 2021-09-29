951815d2f39baf5f78923e116ae15856f648c1d1
ksso730 <ksso7300@gmail.com>
2021-09-30 00:27:36 +0900

    User model추가 및 비밀번호 hashing(bcrypt)

--- src/contorllers/userController.js
+++ src/contorllers/userController.js
@@ -1,7 +1,20 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
+export const postJoin = async(req, res) => {
+    const {name, username, password, email, location} = req.body;
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+    return res.redirect("/login");
+}
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("login User");
+export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("logout User");
 export const see = (req, res) => res.send("See User");
 
---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    username: {type:String, required: true, unique: true},
+    password: {type:String, required: true},
+    name: {type: String, required:true},
+
+});
+
+userSchema.pre('save', async function(){
+    // 비밀번호 hashing
+    this.password = await bcrypt.hash(this.password, 5);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
