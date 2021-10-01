16f94a24eb961e2e0b99dd1e37f77730ff3040c6
ksso730 <ksso7300@gmail.com>
2021-10-01 18:29:39 +0900

    login session 저장

--- src/contorllers/userController.js
+++ src/contorllers/userController.js
@@ -1,27 +1,28 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import session from "express-session";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = async (req, res) => {
+export const postJoin = async(req, res) => {
     const { name, username, password, password2, email, location } = req.body;
     const pageTitle = "Join"
-    if(password !== password2){
+    if (password !== password2) {
         // 브라우저에게 url을 기록하지 말라고 알려주는 방법은 status를 직접 알려주는 것이다.
-        return res.status(400).render("join", { 
+        return res.status(400).render("join", {
             pageTitle,
             errorMessage: "Password confirmation does not match.",
-         });
+        });
     }
 
     const exists = await User.exists({ $or: [{ username }, { email }] });
     if (exists) {
-        return res.status(400).render("join", { 
+        return res.status(400).render("join", {
             pageTitle,
             errorMessage: "This username/email is already taken."
-         });
+        });
     }
 
-    try{
+    try {
         await User.create({
             name,
             username,
@@ -30,7 +31,7 @@ export const postJoin = async (req, res) => {
             location,
         });
         return res.redirect("/login");
-    }catch (error) {
+    } catch (error) {
         console.log(error);
         return res.status(400).render("upload", {
             pageTitle,
@@ -45,26 +46,29 @@ export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" }
 
 export const postLogin = async(req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({username});
-    if(!user){
+    const user = await User.findOne({ username });
+    if (!user) {
         return res
             .status(400).render("login", {
                 pageTitle: "Login",
                 errorMessage: "An account with this username does not exits."
-        });
+            });
     };
     const ok = await bcrypt.compare(password, user.password);
-    if(!ok){
+    if (!ok) {
         return res
             .status(400).render("login", {
                 pageTitle: "Login",
                 errorMessage: "Wrong password",
-        });
+            });
     };
+    req.session.loggedIn = true;
+    // session에 정보 추가
+    console.log(user);
+    req.session.user = user;
 
     return res.redirect("/");
 };
 
 export const logout = (req, res) => res.send("logout User");
 export const see = (req, res) => res.send("See User");
-
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import session from "express-session"
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -11,7 +12,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // session 미들웨어가 서버에 text를 보낸다.
 app.use(
@@ -29,11 +30,12 @@ app.use((req, res, next) => {
     });
 });
 
-app.get("/add-one", (req,res,next)=>{
-    req.session.potato +=1;
-    return res.send(`${req.session.id} ${req.session.potato}` );
+app.get("/add-one", (req, res, next) => {
+    req.session.potato += 1;
+    return res.send(`${req.session.id} ${req.session.potato}`);
 });
 
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
